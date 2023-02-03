1460c3170d514ca07ceb6114fd82e7489e68af25
robertsy26 <robertsy26@up.edu>
2023-02-02 18:17:36 -0800

    2: Checkpoint #2

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -120,8 +120,7 @@ public class CakeView extends SurfaceView {
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
         //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
-        drawCandle(canvas, cakeLeft + cakeWidth * (2.0f/3.0f) - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
 
     }//onDraw
 
